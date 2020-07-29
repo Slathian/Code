352527010aaf6ad232de6e08fd9f37ab2a14736f
Donner <joshua@angelo.net>
2020-07-29 14:46:25 -0700

    calculations done on web page

--- Javascript/TESTING_BENCH.js
+++ Javascript/TESTING_BENCH.js
@@ -1,5 +1,3 @@
-let arr1 = [0,1,2,3];
-let arr2 = [4,5,6,7];
-
-let org = [`This is the full list: ${arr1.concat(arr2)}`];
-console.log(org);
+let arr1 = ['Henry', 'Cunt', 'Joe'];
+let _placeHolder = arr1.indexOf('Joe');
+console.log(_placeHolder);
