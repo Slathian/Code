1ddd924c487e778a9304ede229d84324993d89e1
Donner <joshua@angelo.net>
2020-10-18 12:59:42 -0700

    WORKING BLOG!!!!

--- BLOG_Tutorial/server.js
+++ BLOG_Tutorial/server.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const app = express();
-const articleRouter = require('./routes/articles')
+const mongoose  = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override')
 
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+// this was all in the documentation, if I want to learn more look it up
+const app = express();
+
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
+
+
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt: 'desc'})
+    res.render('articles/index', { articles: articles})
+})
 
 app.use('/articles', articleRouter)
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: "Title 1",
-        createdAt: new Date(),
-        description: 'Test Descriptions 1'
-    },
-    {
-        title: "Title 2",
-        createdAt: new Date(),
-        description: 'Test Descriptions 2'
-    }
-    ]
-    res.render('articles/index', { articles: articles})
-});
 
 app.listen(5000);
 
