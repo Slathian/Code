33400fc45db5a0b32402539286478d7b917bb8e9
Donner <joshua@angelo.net>
2020-12-19 13:40:22 -0800

    took out the packages node_modules

---
+++ React_Class_Dump/FirstTestingReact/TestingTemplate/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>My test application</h1>
+        <h2>This should be fun lmfao</h2>
+      </div>
+    );
+  }
+}
